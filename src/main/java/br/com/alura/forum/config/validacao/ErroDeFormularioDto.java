ca1081926db3df4be651b068533ed9dacf0f43db
gabemanfroi <gabsmanfroi@gmail.com>
2021-02-19 13:01:30 -0300

    First commit

---
+++ src/main/java/br/com/alura/forum/config/validacao/ErroDeFormularioDto.java
@@ -0,0 +1,28 @@
+package br.com.alura.forum.config.validacao;
+
+public class ErroDeFormularioDto {
+	
+	private String campo;
+	private String mensagem;
+	
+	public ErroDeFormularioDto(String campo, String mensagem) {
+		this.campo = campo;
+		this.mensagem = mensagem;
+	}
+	
+	public String getCampo() {
+		return campo;
+	}
+	public void setCampo(String campo) {
+		this.campo = campo;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	
+	
+	
+}
---
+++ src/main/java/br/com/alura/forum/controller/form/AtualizacaoTopicoForm.java
@@ -0,0 +1,40 @@
+package br.com.alura.forum.controller.form;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+
+public class AtualizacaoTopicoForm {
+	
+	@NotNull @NotBlank @Length(min = 10)
+	private String titulo;
+	@NotNull @NotBlank @Length(min = 10)
+	private String mensagem;
+	
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public Topico atualizar(Long id, TopicoRepository topicoRepository) {
+		
+		Topico topico = topicoRepository.getOne(id);
+		topico.setTitulo(this.titulo);
+		topico.setMensagem(this.mensagem);
+		
+		return topico;
+		
+	}
+
+}
